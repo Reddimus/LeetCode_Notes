2074fbf01c95ff2e55f3fd3ae0d46440e21c9aea
Reddimus <Kevi.M.CECS@outlook.com>
2023-06-20 18:56:56 -0700

    Changed file path

--- Arrays & Hashing/LeetCode_49-Group_Anagrams/LeetCode #49 - Group Anagrams.py
+++ Arrays & Hashing/LeetCode_49-Group_Anagrams/LeetCode #49 - Group Anagrams.py
@@ -1,5 +1,6 @@
 '''
 Leetcode #49 - Group Anagrams prompt:
+
 Given an array of strings strs, group the anagrams together. You can return 
 the answer in any order.
 
@@ -20,8 +21,7 @@ Input: strs = ["a"]
 Output: [["a"]]
 
 Constraints:
-
-1 <= strs.length <= 104
+1 <= strs.length <= 10^4
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 '''
