8894e72819d6166dc0e7969108f0c7db7ca8dc33
Reddimus <Kevi.M.CECS@outlook.com>
2023-06-26 17:38:03 -0700

    refactored testcases

--- Arrays & Hashing/LeetCode_242-Valid_Anagram/LeetCode #242 - Valid Anagram.py
+++ Arrays & Hashing/LeetCode_242-Valid_Anagram/LeetCode #242 - Valid Anagram.py
@@ -1,5 +1,6 @@
 '''
 Leetcode #242 - Valid Anagram prompt:
+
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -11,17 +12,15 @@ Output: true
 Example 2:
 Input: s = "rat", t = "car"
 Output: false
- 
 
 Constraints:
-
 1 <= s.length, t.length <= 5 * 10^4
 s and t consist of lowercase English letters.
 '''
 
 class Solution:
 	# Char counting/Hash map approach
-	# T&M: O(n), where n is size of s & t
+	# T & M: O(n), where n is size of largest string of s or t
 	def isAnagram(self, s: str, t: str) -> bool:
 		if len(s) != len(t):
 			return False
@@ -32,8 +31,8 @@ class Solution:
 			t_cnt[t_char] = 1 + t_cnt.get(t_char, 0)
 		return s_cnt == t_cnt
 
-	# Sorting
-	# Time & space complexity: O(n log n)
+	# Sorting approach
+	# T: O(n log n), M: O(1), where n is size of largest string of s or t
 	# def isAnagram(self, s: str, t: str) -> bool:
 	# 	if sorted(s) == sorted(t):
 	# 		return True
@@ -42,11 +41,11 @@ class Solution:
 s = Solution()
 
 # Ex1
-ans = s.isAnagram(s = "anagram", t = "nagaram")
-assert ans == True, f'Expected True but got {ans}'
+attempt = s.isAnagram(s = "anagram", t = "nagaram")
+assert attempt == True, f'Expected True but got {attempt}'
 # Ex2
 ans = s.isAnagram(s = "rat", t = "car")
-assert ans == False, f'Expected False but got {ans}'
+assert attempt == False, f'Expected False but got {attempt}'
 # Ex3
-ans = s.isAnagram(s = "Hello", t = "Hell")
-assert ans == False, f'Expected False but got {ans}'
+ans = s.isAnagram(s = "Hello", t = "He")
+assert attempt == False, f'Expected False but got {attempt}'
