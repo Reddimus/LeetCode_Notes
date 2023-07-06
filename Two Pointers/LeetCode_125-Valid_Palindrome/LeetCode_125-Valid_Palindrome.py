e8df37e942a7719d9af9d6399fefca5e670836ed
Reddimus <Kevi.M.CECS@outlook.com>
2023-07-06 10:22:56 -0700

    Renamed files

--- Two Pointers/LeetCode_125-Valid_Palindrome/LeetCode_125-Valid_Palindrome.py
+++ Two Pointers/LeetCode_125-Valid_Palindrome/LeetCode_125-Valid_Palindrome.py
@@ -1,4 +1,6 @@
 '''
+LeetCode #11 - Valid Palindrome prompt:
+
 A phrase is a palindrome if, after converting all uppercase letters into 
 lowercase letters and removing all non-alphanumeric characters, it reads 
 the same forward and backward. Alphanumeric characters include letters 
@@ -30,9 +32,30 @@ Constraints:
 '''
 
 class Solution:
+	# Two pointers method
+	# T: O(n), M: O(1), where n is size of s
+	def isPalindrome(self, s: str) -> bool:
+		lo, hi = 0, len(s) - 1
+		# converge pointers and compare
+		while lo < hi:
+			if not s[lo].isalnum():
+				lo += 1
+				continue
+			if not s[hi].isalnum():
+				hi -= 1
+				continue
+			if s[lo].lower() != s[hi].lower():
+				return False
+			lo += 1
+			hi -= 1
+		return True
+
+	'''
+	# Other way to iterate two pointers method
 	# T: O(n), M: O(1), where n is size of s
 	def isPalindrome(self, s: str) -> bool:
 		l_idx, r_idx = 0, len(s) - 1
+		# converge pointers and compare
 		while l_idx < r_idx:
 			while l_idx < r_idx and not self.alphanum(s[l_idx]):
 				l_idx += 1
@@ -50,55 +73,21 @@ class Solution:
 			or '0' <= char <= '9'
 			)
 	'''
-	# similar algorithm, VERY close in runtime but a little slower
-	# Time complexity: 	O(n)
-	# Space complexity: O(1)
-	def isPalindrome(self, s: str) -> bool:
-		l_idx, r_idx = 0, len(s) - 1
-		
-		# iterate from left & right side of the str
-		while(l_idx < r_idx):	
-			# skip non-alphabetical chars or nums
-			l_idx = self.skip_char(s, l_idx, reverse = False)
-			r_idx = self.skip_char(s, r_idx, reverse = True)
-			if s[l_idx].lower() != s[r_idx].lower() and l_idx < r_idx:
-				return False
-			l_idx += 1
-			r_idx -= 1
-		return True
-		
-	def skip_char(self, s: str, idx: int, reverse: bool):
-		while ((s[idx].lower() < 'a') or ('z' < s[idx].lower())) and (((s[idx] < '0')) or ('9' < s[idx])):
-			# if not alpha char or number skip
-			if reverse == False and idx + 1 <= len(s) - 1:
-				idx += 1
-			elif reverse == True and idx - 1 >= 0:
-				idx -= 1
-			else:
-				break
-		return idx
-	'''
 
-# My Ex 1
-assert Solution().isPalindrome(s = "cabbac") == True
-
-# My Ex 2
-assert Solution().isPalindrome(s = "cac") == True
-
-# My Ex 3
-assert Solution().isPalindrome(s = "012210") == True
-
-# My Ex 4
-assert Solution().isPalindrome(s = "04104214") == False
-
-# My Ex 5
-assert Solution().isPalindrome(s = '.,') == True
-
+sol = Solution()
 # Ex 1
-assert Solution().isPalindrome(s = "A man, a plan, a canal: Panama") == True
-
+assert sol.isPalindrome(s = "A man, a plan, a canal: Panama") == True, f'Expected True but got False'
 # Ex 2
-assert Solution().isPalindrome(s = "race a car") == False
-
+assert sol.isPalindrome(s = "race a car") == False, f'Expected False but got True'
 # Ex 3
-assert Solution().isPalindrome(s = '') == True
+assert sol.isPalindrome(s = '') == True, f'Expected True but got False'
+# My Ex 1
+assert sol.isPalindrome(s = "cabbac") == True, f'Expected True but got False'
+# My Ex 2
+assert sol.isPalindrome(s = "cac") == True, f'Expected True but got False'
+# My Ex 3
+assert sol.isPalindrome(s = "012210") == True, f'Expected True but got False'
+# My Ex 4
+assert sol.isPalindrome(s = "04104214") == False, f'Expected False but got True'
+# My Ex 5
+assert sol.isPalindrome(s = '.,') == True, f'Expected True but got False'
