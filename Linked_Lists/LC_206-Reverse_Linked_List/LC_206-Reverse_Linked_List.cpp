d9e24aae811de77867e515fc145a6e9f6e942783
Reddimus <Kevi.M.CECS@outlook.com>
2023-09-11 22:51:35 -0700

    Added C++ Solution

---
+++ Linked_Lists/LC_206-Reverse_Linked_List/LC_206-Reverse_Linked_List.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	// Iterative solution
+	// T: O(n), M: O(1), where n is size of linked list input
+	ListNode* reverseList(ListNode* head) {
+		ListNode *prev = NULL;
+		ListNode *curr = head;
+
+		while (curr) {
+			ListNode *temp = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = temp;
+		}
+		return prev;
+	}
+
+	/*
+	// Recursive solution
+	// T & M: O(n), where n is size of linked list input
+	ListNode* reverseList(ListNode* head) {
+		if (!head || !head->next)
+			return head;
+
+		ListNode *newHead = reverseList(head->next);
+		head->next->next = head;
+		head->next = NULL;
+
+		return newHead;
+	}
+	*/
+
+};
+
+void testCase(ListNode *head, vector<int> ans) {
+	vector<int> attempt;
+	ListNode *curr = Solution().reverseList(head);
+	while (curr) {
+		attempt.push_back(curr->val);
+		curr = curr->next;
+	}
+	// print attempt vector
+	cout << "Attempt: ";
+	for (int i = 0; i < attempt.size(); i++) {
+		cout << attempt[i] << " ";
+	}
+	cout << endl;
+
+	// print ans vector
+	cout << "Ans: ";
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] << " ";
+	}
+	cout << endl << endl;
+	
+	assert(attempt == ans);
+}
+
+int main() {
+	ListNode *head;
+	vector<int> ans;
+
+	// Example 1
+	head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+	ans = {5, 4, 3, 2, 1};
+	testCase(head, ans);
+	// Example 2
+	head = new ListNode(1, new ListNode(2));
+	ans = {2, 1};
+	testCase(head, ans);
+	// Example 3
+	head = nullptr;
+	ans = {};
+	testCase(head, ans);
+
+	return 0;
+}
