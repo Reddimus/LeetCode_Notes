195febb2c696508631e9c595ed85a62c3e0de22d
Reddimus <Kevi.M.CECS@outlook.com>
2023-09-17 01:51:54 -0700

    Added C++ solution

---
+++ 1-D_Dynamic_Programming/Easy/LC_509-Fibonacci_Number/LC_509-Fibonacci_Number.cpp
@@ -0,0 +1,34 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1)
+            return n;
+        
+        // Start from 2
+        int n0 = 0, n1 = 1;
+        for (int num = 2; num <= n; num++) {
+            int temp = n0 + n1;
+            n0 = n1;
+            n1 = temp;
+        }
+        return n1;
+    }
+};
+
+int main() {
+    Solution sol;
+    int attempt;
+    
+    // Ex1
+    assert(sol.fib(2) == 1);
+    // Ex2
+    assert(sol.fib(3) == 2);
+    // Ex3
+    assert(sol.fib(4) == 3);
+}
