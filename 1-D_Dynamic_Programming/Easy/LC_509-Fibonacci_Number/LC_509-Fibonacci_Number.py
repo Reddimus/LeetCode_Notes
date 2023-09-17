caf948737ea51a16c00b374914308adc2cee963a
Reddimus <Kevi.M.CECS@outlook.com>
2023-09-17 01:48:27 -0700

    Added python solution

---
+++ 1-D_Dynamic_Programming/Easy/LC_509-Fibonacci_Number/LC_509-Fibonacci_Number.py
@@ -0,0 +1,23 @@
+class Solution:
+    # T: O(n), M: O(1)
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        
+        # Start from 2
+        n0, n1 = 0, 1
+        for num in range(2, n+1):
+            n0, n1 = n1, n0 + n1
+        return n1
+
+sol = Solution()
+
+# Ex1
+attempt = sol.fib(2)
+assert attempt == 1, "Expected 1, but got: " + attempt
+# Ex2
+attempt = sol.fib(3)
+assert attempt == 2, "Expected 2, but got: " + attempt
+# Ex3
+attempt = sol.fib(4)
+assert attempt == 3, "Expected 3, but got: " + attempt
