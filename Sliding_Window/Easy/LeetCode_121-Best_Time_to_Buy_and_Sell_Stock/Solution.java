f886c40af2c5d598337dfca8d7ae27658f09ac17
Reddimus <Kevi.M.CECS@outlook.com>
2023-09-28 12:12:06 -0700

    Added java file

---
+++ Sliding_Window/Easy/LeetCode_121-Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+    }
+}
+
+class TestCases {
+    public static void main(String[] args) {
+
+    }
+}
