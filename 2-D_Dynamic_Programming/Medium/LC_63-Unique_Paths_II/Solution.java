ae2e4630e89b9a4e038c7d80f67018c090e680fe
Reddimus <Kevi.M.CECS@outlook.com>
2023-10-14 18:13:30 -0700

    Added java solutions

--- 2-D_Dynamic_Programming/Medium/LC_63-Unique_Paths_II/Solution.java
+++ 2-D_Dynamic_Programming/Medium/LC_63-Unique_Paths_II/Solution.java
@@ -1,23 +1,24 @@
 class Solution {
     // Bottom-up approach
     // T: O(m*n), M: O(n), where m = rows, n = cols
-    // public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    //     int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
-    //     int[] dp = new int[cols];
-    //     dp[cols-1] = 1; // goal base case
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
+        int[] dp = new int[cols];
+        dp[cols-1] = 1; // goal base case
 
-    //     for (int r = rows-1; r >= 0; --r) {
-    //         for (int c = cols-1; c >= 0; --c) {
-    //             if (obstacleGrid[r][c] == 1)    // obstacle
-    //                 dp[c] = 0;                      // reset steps to 0
-    //             else if (c < cols-1)
-    //                 dp[c] += dp[c+1];
-    //         }
-    //     }
+        for (int r = rows-1; r >= 0; --r) {
+            for (int c = cols-1; c >= 0; --c) {
+                if (obstacleGrid[r][c] == 1)    // obstacle
+                    dp[c] = 0;                      // reset steps to 0
+                else if (c < cols-1)
+                    dp[c] += dp[c+1];
+            }
+        }
 
-    //     return dp[0];
-    // }
+        return dp[0];
+    }
 
+    /*
     // Bottom-Up approach with full dp grid
     // T: O(m*n), M: O(m*n), where m = rows, n = cols
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -36,6 +37,47 @@ class Solution {
 
         return dp[0][0];
     }
+    */
+
+    /*
+    // Top-Down Approach
+    // T: O(m*n), M: O(n), where m = rows, n = cols
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
+        int[] dp = new int[cols];
+        dp[0] = 1; // start base case
+
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c)
+                if (obstacleGrid[r][c] == 1)    // obstacle
+                    dp[c] = 0;                      // reset steps to 0
+                else if (c > 0)
+                    dp[c] += dp[c-1];
+        }
+
+        return dp[cols-1];
+    }
+    */
+
+    /*
+    // Top-Down Approach with full dp grid
+    // T: O(m*n), M: O(m*n), where m = rows, n = cols
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
+        int[][] dp = new int[rows+1][cols+1];
+        dp[1][1] = 1; // start base case
+
+        for (int r = 1; r <= rows; ++r) {
+            for (int c = 1; c <= cols; ++c)
+                if (obstacleGrid[r-1][c-1] == 1)    // obstacle
+                    dp[r][c] = 0;                      // reset steps to 0
+                else
+                    dp[r][c] += dp[r-1][c] + dp[r][c-1];
+        }
+
+        return dp[rows][cols];
+    }
+    */
 }
 
 class TestCases {
