b0ada1b1aa5dcb05db37756dbdf9e10d5c6b64e3
Reddimus <Kevi.M.CECS@outlook.com>
2023-11-07 21:26:21 -0800

    Added Full Grid/Bottom-Up Approach

--- 2-D_Dynamic_Programming/Medium/LC_62-Unique_Paths/Solution.java
+++ 2-D_Dynamic_Programming/Medium/LC_62-Unique_Paths/Solution.java
@@ -1,6 +1,22 @@
 class Solution {
+    // 2-D Dynamic Programming - Full Grid/Bottom-Up Approach
     public int uniquePaths(int m, int n) {
-        return -1;
+        int[][] dp = new int[m][n];
+        
+        // Solve base case; last row and last column
+        for (int col = 0; col < n; ++col)
+            dp[m-1][col] = 1;
+        for (int row = 0; row < m; ++row)
+            dp[row][n-1] = 1;
+
+        // Solve remaining cases using bottom up approach
+        for (int row = m - 2; 0 <= row; --row) {
+            for (int col = n - 2; 0 <= col; --col) {
+                dp[row][col] = dp[row][col+1] + dp[row+1][col];
+            }
+        }
+
+        return dp[0][0];
     }
 }
 
