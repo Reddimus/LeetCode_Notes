5309643e953f9e7d24517e3cb844136ba764f037
Reddimus <Kevi.M.CECS@outlook.com>
2024-03-02 18:38:42 -0800

    Added src files

---
+++ Heap-Priority_Queue/Medium/LC_973-K_Closest_Points_to_Origin/LC_973-K_Closest_Points_to_Origin.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    kClosest() {
+        
+    }
+};
+
+int main() {
+
+}
---
+++ Heap-Priority_Queue/Medium/LC_973-K_Closest_Points_to_Origin/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    kClosest() {
+        
+    }
+}
+
+class TestCases {
+    public static void main(String[] args) {
+        
+    }
+}
